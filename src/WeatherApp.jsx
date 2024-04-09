258e7d7216359b12669e9cd407493e6ef5cdba57
spezdmtest <spezd.m.@gmail.com>
2024-04-09 16:27:43 +0300

    add state togglePopup

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-// import Loader from "./img/loader.gif";
+import Loader from "/img/loader.gif";
 
 const WeatherApp = () => {
   const [city, setCity] = useState("");
@@ -13,10 +13,45 @@ const WeatherApp = () => {
 
   const fetchData = async () => {
     try {
-      // const query = loacalStorage
-    } catch {}
+      const query = localStorage.getItem("query") || "Romny";
+      const response = await fetch(
+        `http://api.weatherstack.com/current?access_key=7b048faeac1477bce5ee8a11ba24330e&query=${query}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (err) {
+      console.log("Error fetching data: ", err);
+    }
   };
-  return <div id="app"></div>;
+
+  const togglePopup = () => {
+    setPopupActive(!popupActive);
+  };
+
+  return (
+    <div id="weather-app">
+      <div id="root">
+        {weatherData ? (
+          <div className="container">
+            <div className="top">
+              <div className="city">
+                <div className="city-subtitle">Weather Today in</div>
+                <div className="city-title" id="city" onClick={togglePopup}>
+                  <span>{weatherData.location.name}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <img
+            className="loader"
+            src={Loader}
+            alt="loader"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 // function WeatherApp() {
