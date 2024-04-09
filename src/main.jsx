0b349d309275cbe9ea470906b9f7a651c373edc0
spezdmtest <spezd.m.@gmail.com>
2024-04-09 12:01:26 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Weather from "./WeatherApp.jsx";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Weather />);
